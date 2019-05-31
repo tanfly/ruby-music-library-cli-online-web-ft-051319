98c93c6cb9e68aacc67c01dcd95a4798146b1125
Stella McEwen <mcewen.stella@gmail.com>
2019-05-31 16:19:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,17 +3,26 @@ class Artist
   extend Concerns::Findable
 
   
-  attr_accessor :name, :songs, :genres 
+  attr_accessor :name, :genres 
   
   @@all = []
   
   def initialize(name)
     @name = name
-    @songs = []
+  
   end
   
   def self.all 
     @@all 
+  end
+
+  #beyonce.songs
+  #beyonce => <Artist3495834509 @name="Beyonce
+
+  def songs 
+    Songs.all.select {|song| song.artist == self} 
+      
+    #=> [<Song90348503945 @artist = <Artist3495834509 @name="Beyonce">>, <Song349583054 @artist=<Artist34958309548 @name="Adele">>, <Song30948503845]
   end
   
   def self.create(artist) 
