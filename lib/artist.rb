edf346e1923cf3601905aecb022a11b0f1e03f79
Stella McEwen <mcewen.stella@gmail.com>
2019-05-31 16:19:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,58 +1,58 @@
 
-class Artist 
+class Artist
   extend Concerns::Findable
 
-  
-  attr_accessor :name, :genres 
-  
+
+  attr_accessor :name, :genres
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
-  
+
   end
-  
-  def self.all 
-    @@all 
+
+  def self.all
+    @@all
   end
 
   #beyonce.songs
   #beyonce => <Artist3495834509 @name="Beyonce
 
-  def songs 
+  def songs
     Songs.all.select {|song| song.artist == self} 
-      
+
     #=> [<Song90348503945 @artist = <Artist3495834509 @name="Beyonce">>, <Song349583054 @artist=<Artist34958309548 @name="Adele">>, <Song30948503845]
   end
-  
-  def self.create(artist) 
+
+  def self.create(artist)
     @artist = self.new(artist)
-    @artist.save 
-    @artist 
+    @artist.save
+    @artist
   end
-  
-  def self.destroy_all 
-    self.all.clear 
+
+  def self.destroy_all
+    self.all.clear
   end
-  
-   def self.count 
-    @@all.size 
+
+   def self.count
+    @@all.size
   end
-  
-  def save 
-    @@all << self 
+
+  def save
+    @@all << self
   end
-  
+
   def add_song(song)
     self.songs << song unless songs.include?(song)
-    if song.artist.nil? 
-      song.artist = self 
+    if song.artist.nil?
+      song.artist = self
     end
   end
-  
-  def genres 
+
+  def genres
     songs.collect {|song| song.genre}.uniq
   end
-        
-    
-end 
+
+
+end
